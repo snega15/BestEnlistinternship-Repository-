99e60e7a98aab1aeae8c0e56a586ea78f554a803
snega15 <75003695+snega15@users.noreply.github.com>
2020-11-25 18:44:36 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,22 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=b=c=50
+>>> print(a/10)
+5.0
+>>> print(b*50)
+2500
+>>> print(c+60)
+110
+>>> string="kujhy"
+>>> string=string.replace("j","G")
+>>> print(string)
+kuGhy
+>>> a=int(1000)
+>>> a=a/3
+>>> print(a)
+333.3333333333333
+>>> b=float(2.5)
+>>> b=b*4
+>>> print(b)
+10.0
+>>> 
