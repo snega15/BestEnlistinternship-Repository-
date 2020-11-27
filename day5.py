09cb59945fa982cd68f2535d8f19dd75d676d498
snega15 <75003695+snega15@users.noreply.github.com>
2020-11-27 16:42:21 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,42 @@
+def sum(x,y):
+    return x+y
+
+a=int(input("Enter num 1="))
+b=int(input("Enter num 2="))
+c=sum(a,b)
+print("sum of",a,"and",b,"is",c)
+
+#subtraction
+def subtraction(x,y):
+    return x-y
+
+c=subtraction(a,b)
+print("sub of",a,"and",b,"is",c)
+
+  #division
+def division(x,y):
+    return x/y
+
+c=division(a,b)
+print("div of",a,"and",b,"is",c)
+
+
+  #multiplication
+def mul(x,y):
+    return x*y
+
+c=mul(a,b)
+print("mul of",a,"and",b,"is",c)
+
+  #covid()
+def changeme(mylist):
+    "This changes a passed list into this function"
+    mylist.append(["temp 98 degree"]);
+    print("covid():",mylist)
+    return
+
+mylist["suresh","ramesh","jeya","judi","kelan"];
+changeme(mylist);
+print("covid():",mylist)
+  
+
